37159d7773f64b3b0c12c426f42bd165b493358f
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-05-20 00:25:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,43 +7,6 @@ function mostrarSituacao(media){
 }
 mostrarSituacao(8)
 
-function confirma(){
-    resposta = confirm('Dê ok para confirmar')
-    if (resposta==1){
-        return true
-    }else{
-        return false
-    }
-}
-confirma()
-
-function entradaDeDados(){
-    nome = prompt('Digite seu nome:')
-    return nome
-}
-entradaDeDados()
-
-function perguntaEndereco(){
-    do {
-        endereco = prompt('Digite seu endereço:')
-        confirma = confirm(`Seu endereço é ${endereco} ?`)
-    }while(!confirma)
-        alert('O endereço será exibido na página...')
-        document.write(`Seu endererço é ${endereco}`)
-}
-perguntaEndereco()
-
-
-
-scriptjs
-function mostrarSituacao(media){
-    if(media >=7){
-        alert('Você foi aprovado');
-    }else{
-        alert('Você foi reprovado :(');
-    }
-}
-mostrarSituacao(8)
 
 function confirma(){
     resposta = confirm('Dê ok para confirmar')
@@ -55,11 +18,13 @@ function confirma(){
 }
 confirma()
 
+
 function entradaDeDados(){
     nome = prompt('Digite seu nome:')
     return nome
 }
 entradaDeDados()
+
 
 function perguntaEndereco(){
     do {
